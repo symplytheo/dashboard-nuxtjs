c9949acb948d902f98c6c001512c3c213b843323
Theo Iyonor <symplytheo@gmail.com>
2020-10-08 19:15:42 +0100

    Utitlize material-card component

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-container fluid>
+    <v-container>
       <v-row>
         <v-col
           v-for="(item, i) in alerts"
@@ -10,111 +10,128 @@
           lg="4"
           class="py-5"
         >
-          <v-card class="px-2">
-            <v-row>
-              <v-col cols="5" sm="4" class="pb-0 mb-0">
-                <v-card
-                  class="v-sheet--offset mx-auto py-5 text-center"
-                  color="primary"
-                  elevation="6"
-                  width="100%"
-                >
-                  <v-icon size="50" color="white">{{ item.icon }}</v-icon>
-                </v-card>
-              </v-col>
-              <v-col cols="7" sm="8" class="text-right pa-5 pb-0 primary--text">
-                <div class="subtitle-1 font-weight-bold text-uppercase">
-                  {{ item.title }}
-                </div>
-                <div class="display-1 font-weight-bold">
-                  {{ item.text }}
-                </div>
-              </v-col>
-            </v-row>
-            <v-card-text class="px-0 pt-0">
-              <v-divider />
-              <div class="pt-2">
-                <v-icon left small color="primary">mdi-clock</v-icon>
-                Updated at {{ Math.ceil(Math.random() * 6) }} minutes ago
-              </div>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-    <v-container fluid>
-      <v-row>
-        <v-col v-for="w in 3" :key="w" cols="12" md="4">
-          <v-card class="mt-4 mx-auto" max-width="400">
-            <v-sheet
-              class="v-sheet--offset mx-auto"
-              color="primary"
-              elevation="12"
-              max-width="calc(100% - 32px)"
-            >
-              <v-sparkline
-                :labels="labels"
-                :value="value"
-                color="white"
-                line-width="2"
-                padding="16"
-                class="white--text"
-              ></v-sparkline>
-            </v-sheet>
-
-            <v-card-text class="pt-0">
-              <div class="title font-weight-light mb-2">User Registrations</div>
-              <div class="subheading font-weight-light grey--text">
-                Last Campaign Performance
-              </div>
-              <v-divider class="my-2"></v-divider>
-              <v-icon class="mr-2" small> mdi-clock </v-icon>
-              <span class="caption grey--text font-weight-light">
-                last registration 26 minutes ago
-              </span>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-    <v-container fluid>
-      <v-row>
-        <v-col v-for="i in 2" :key="i" cols="12" md="6">
-          <v-card
-            class="mx-auto text-center"
-            color="primary"
-            dark
-            max-width="600"
+          <material-card
+            :icon="item.icon"
+            :color="item.color"
+            :title="item.title"
+            :subtitle="item.subtitle"
+            footer
           >
-            <v-card-text>
-              <v-sheet color="rgba(0,0,0,.12)">
-                <v-sparkline
-                  :value="value_2"
-                  color="white"
-                  height="100"
-                  padding="24"
-                  stroke-linecap="round"
-                  smooth
-                  fill
-                  class="white--text"
-                >
-                  <template v-slot:label="item"> ${{ item.value }} </template>
-                </v-sparkline>
-              </v-sheet>
-            </v-card-text>
-
-            <v-card-text>
-              <div class="headline font-weight-thin">Sales Last 24h</div>
-            </v-card-text>
-
-            <v-divider />
-
-            <v-card-actions class="justify-center">
-              <v-btn block text> Go to Report {{ i }} </v-btn>
-            </v-card-actions>
-          </v-card>
+            <template #footer>
+              <v-icon left small :color="item.footer_icon_color">
+                {{ item.footer_icon }}
+              </v-icon>
+              <span> {{ item.footer_text }} </span>
+            </template>
+          </material-card>
         </v-col>
       </v-row>
+    </v-container>
+
+    <v-container>
+      <v-row>
+        <v-col
+          v-for="item in stats.slice(0, 2)"
+          :key="item.id"
+          cols="12"
+          md="6"
+        >
+          <material-card :color="item.color" footer>
+            <template v-slot:header>
+              <div class="pa-3">
+                <v-sparkline
+                  :labels="item.labels"
+                  :value="item.value"
+                  color="white"
+                  :line-width="item.line_width"
+                  smooth
+                  height="100"
+                  stroke-linecap="round"
+                  :type="item.type"
+                  padding="16"
+                ></v-sparkline>
+              </div>
+            </template>
+            <div class="title font-weight-light mb-2">{{ item.title }}</div>
+            <div class="subtitle-1 font-weight-light grey--text">
+              {{ item.subtitle }}
+            </div>
+            <template #footer>
+              <v-icon class="mr-2" small> {{ item.footer_icon }} </v-icon>
+              <span class="grey--text font-weight-light">
+                {{ item.footer_text }}
+              </span>
+            </template>
+          </material-card>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-container>
+      <v-row>
+        <v-col
+          v-for="stat in stats.slice(2, 4)"
+          :key="stat.id"
+          cols="12"
+          md="6"
+        >
+          <material-card :background="stat.color" stats footer dark>
+            <template #stats>
+              <v-sparkline
+                :value="stat.value"
+                color="white"
+                height="100"
+                padding="20"
+                stroke-linecap="round"
+                class="white--text"
+                smooth
+                fill
+              >
+                <template v-slot:label="item"> ${{ item.value }} </template>
+              </v-sparkline>
+            </template>
+
+            <div class="text-center py-3">
+              <div class="headline font-weight-thin">{{ stat.title }}</div>
+            </div>
+
+            <template #footer>
+              <v-btn block text> Go to Report </v-btn>
+            </template>
+          </material-card>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-container fluid>
+      <div class="headline font-weight-regular ma-5">Manage Listing</div>
+      <v-slide-group>
+        <v-slide-item v-for="(item, m) in 8" :key="m" class="ma-5">
+          <material-card :width="350" footer>
+            <template v-slot:header>
+              <v-card flat>
+                <v-img src="/bg-3.jpg" height="175" />
+              </v-card>
+            </template>
+            <div class="text-center py-2">
+              <div class="body-1 text--primary">Cozy 5 Stars Apartment</div>
+              <div class="body-2 font-weight-thin">
+                This place is close to barcelona beach and is just 2 minutes
+                walk away and also near the best night club in the town.
+              </div>
+            </div>
+            <template #footer>
+              <div>
+                <span class="subtitle-1">$899/night</span>
+                <span class="float-right caption">
+                  <v-icon x-small>mdi-map-marker</v-icon>
+                  Barcelona, Spain
+                </span>
+              </div>
+            </template>
+          </material-card>
+        </v-slide-item>
+      </v-slide-group>
     </v-container>
   </div>
 </template>
@@ -124,31 +141,92 @@ export default {
   data: () => ({
     alerts: [
       {
-        title: 'Categories',
-        text: 43,
-        icon: 'mdi-tag-multiple',
+        title: 'Followers',
+        subtitle: '+245',
+        icon: 'mdi-twitter',
+        color: 'blue lighten-1',
+        footer_icon: 'mdi-clock',
+        footer_icon_color: '',
+        footer_text: 'Just updated',
       },
       {
-        title: 'Facilities',
-        text: 34,
-        icon: 'mdi-office-building',
+        title: 'Website Visits',
+        subtitle: 75.521,
+        icon: 'mdi-chart-bar',
+        color: 'pink',
+        footer_icon: 'mdi-tag',
+        footer_icon_color: '',
+        footer_text: 'Tracked from Google Analytics',
       },
       {
-        title: 'Users',
-        text: 24,
-        icon: 'mdi-account-multiple',
+        title: 'Revenue',
+        subtitle: '$ 34,245',
+        icon: 'mdi-cash-multiple',
+        color: 'green',
+        footer_icon: 'mdi-rocket-launch',
+        footer_icon_color: 'green',
+        footer_text: 'Last 24 Hours',
+      },
+      {
+        title: 'Bookings',
+        subtitle: '184',
+        icon: 'mdi-sofa',
+        color: 'amber darken-3',
+        footer_icon: 'mdi-alert',
+        footer_icon_color: 'error',
+        footer_text: 'Get More Space...',
       },
     ],
-    labels: ['12am', '3am', '6am', '9am', '12pm', '3pm', '6pm', '9pm'],
-    value: [200, 675, 410, 390, 310, 460, 250, 240],
-    value_2: [423, 446, 675, 510, 590, 610, 760],
+    stats: [
+      {
+        id: 1,
+        title: 'Website Views',
+        subtitle: 'Last Campaign Performance',
+        color: 'pink',
+        labels: ['J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'],
+        value: [400, 500, 450, 200, 675, 410, 390, 310, 460, 250, 240, 768],
+        line_width: 15,
+        type: 'bars',
+        footer_icon: 'mdi-clock',
+        footer_text: 'updated 10 minutes ago',
+      },
+      {
+        id: 2,
+        title: 'Daily Sales',
+        subtitle: "55% increase in today's sales",
+        color: 'green',
+        labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+        value: [10, 14, 6, 18, 22, 19, 39],
+        line_width: 2,
+        type: 'trend',
+        footer_icon: 'mdi-clock',
+        footer_text: 'updated 10 minutes ago',
+      },
+      {
+        id: 3,
+        title: 'Daily Sales',
+        subtitle: "55% increase in today's sales",
+        color: 'amber darken-3',
+        labels: ['12am', '3am', '6am', '9am', '12pm', '3pm', '6pm', '9pm'],
+        value: [200, 675, 410, 390, 310, 460, 250, 240],
+        line_width: 2,
+        type: 'trend',
+        footer_icon: 'mdi-clock',
+        footer_text: 'updated 10 minutes ago',
+      },
+      {
+        id: 4,
+        title: 'Daily Sales',
+        subtitle: "55% increase in today's sales",
+        color: 'blue lighten-2',
+        labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+        value: [10, 14, 6, 18, 22, 19, 39],
+        line_width: 2,
+        type: 'trend',
+        footer_icon: 'mdi-clock',
+        footer_text: 'updated 10 minutes ago',
+      },
+    ],
   }),
 }
 </script>
-
-<style>
-.v-sheet--offset {
-  top: -24px;
-  position: relative;
-}
-</style>
